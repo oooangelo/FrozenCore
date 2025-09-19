3fbe0a3e76a9c18ae76173b784805ea0777b474b
angelo <a.angelozuzi@gmail.com>
2025-09-19 20:04:53 -0300

    change main code to: game and menu, main loop

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-import pygame
+from code.Game import Game
 
-print('setup start')
-
-pygame.init()
-
-window = pygame.display.set_mode(size=(1280, 720))
-
-print('setup end')
-
-print('setup loop')
-
-while True:
-    #event check
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
+game = Game()
+game.run()
